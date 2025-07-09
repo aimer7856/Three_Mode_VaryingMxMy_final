131949fec45ed232f1017a1ad79a483efb4f07eb
aimer7856 <aimer7856@gmail.com>
2025-07-09 13:48:10 -0300

    small mistakes line 36

--- cq_coherent.sh
+++ cq_coherent.sh
@@ -33,7 +33,7 @@ export MEM_LOG_FILE="${OUT_DIR}/mem_log.txt"
 RUN_LOG="${OUT_DIR}/run.log"
 START=$(date +%s)
 
-ppython RunSimulation_coherent.py \
+python RunSimulation_coherent.py \
   --mode "$MODE" --mx "$MX" --my "$MY" \
   --x0 "$X0" --vx0 "$VX0" \
   --nx "$NX" --xmin "$XMIN" --xmax "$XMAX" \
